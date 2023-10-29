eb899e01697e3ab48796bceb179dd894b429a17a
mink7 <mink721@gmail.com>
2023-10-30 00:18:11 +0900

    custom hook

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+import { queryClient } from '../../react-query/queryClient';
 import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
@@ -8,10 +11,13 @@ import { Routes } from './Routes';
 
 export function App(): ReactElement {
   return (
-    <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
